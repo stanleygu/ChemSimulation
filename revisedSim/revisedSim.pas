d5a959bb6746d3bc9d8c13faab0c06ba1a11c2a9
Greg Valainis <valainisgt@gmail.com>
2012-07-10 15:09:38 -0700

    my project

---
+++ revisedSim/revisedSim.pas
@@ -0,0 +1,15 @@
+program revisedSim;
+
+uses
+  cwstring, cthreads, FMX_Forms,
+  uMain in 'uMain.pas' {Form1},
+  uSpecies in 'uSpecies.pas',
+  uReaction in 'uReaction.pas';
+
+{.$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
